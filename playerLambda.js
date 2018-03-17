29ffb363205ebfee6da44700d0cc5fa565cb35b2
Adam Simpson <adam@heysparkbox.com>
2018-03-16 23:14:28 -0400

    feat: cleanup API a bit

--- playerLambda.js
+++ playerLambda.js
@@ -6,24 +6,46 @@ const aws = require('aws-sdk');
 const s3 = new aws.S3({ region: 'us-east-1' });
 
 const playerMap = {
-  steph: 'c/curryst01',
-  harden: 'h/hardeja01',
-  davis: 'd/davisan02',
+  steph: {
+    url: 'c/curryst01',
+    displayName: 'Steph',
+  },
+  harden: {
+    url: 'h/hardeja01',
+    displayName: 'Harden',
+  },
+  davis: {
+    url: 'd/davisan02',
+    displayName: 'AD',
+  },
+  lebron: {
+    url: 'j/jamesle01',
+    displayName: 'LeBron',
+  },
+  dame: {
+    url: 'l/lillada01',
+    displayName: 'Dame',
+  },
+  freak: {
+    url: 'a/antetgi01',
+    displayName: 'Giannis',
+  },
 };
 
 const playerLambda = (event, context, callback) => {
   const gameLog = `https://www.basketball-reference.com/players/${playerMap[
     event.player
-  ]}/gamelog/2018`;
+  ].url}/gamelog/2018`;
 
   const avg = `https://www.basketball-reference.com/players/${playerMap[
     event.player
-  ]}.html`;
+  ].url}.html`;
 
   Promise.all([perGame(gameLog), currentAvgs(avg)]).then(x => {
     const data = {
       perGame: x[0],
       avgs: x[1],
+      displayName: playerMap[event.player].displayName,
     };
     const params = {
       Bucket: process.env.bucket,
@@ -53,22 +75,22 @@ function perGame(url) {
   return new Promise(function(resolve, reject) {
     request(url).then(html => {
       const $ = cheerio.load(html);
-      const games = $('#pgl_basic tbody tr').not('.thead').toArray().map(e => {
-        return {
-          points: $("[data-stat='pts']", e).text() === ''
-            ? 'NA'
-            : $("[data-stat='pts']", e).text(),
-          opp: $("[data-stat='opp_id']", e).text() === ''
-            ? 'NA'
-            : $("[data-stat='opp_id']", e).text(),
-          assists: $("[data-stat='ast']", e).text() === ''
-            ? 'NA'
-            : $("[data-stat='ast']", e).text(),
-          rebounds: $("[data-stat='trb']", e).text() === ''
-            ? 'NA'
-            : $("[data-stat='trb']", e).text(),
-        };
-      });
+      const games = $('#pgl_basic tbody tr')
+        .not('.thead')
+        .toArray()
+        .map(e => {
+          if ($("[data-stat='pts']", e).text() === '') {
+            return null;
+          } else {
+            return {
+              points: $("[data-stat='pts']", e).text(),
+              opp: $("[data-stat='opp_id']", e).text(),
+              assists: $("[data-stat='ast']", e).text(),
+              rebounds: $("[data-stat='trb']", e).text(),
+            };
+          }
+        })
+        .filter(x => x);
       resolve(games);
     });
   });
