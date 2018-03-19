c9c8b98f9b421cce48516b6f3c19f2f03ba6a495
Adam Simpson <adam@heysparkbox.com>
2018-03-19 00:15:01 -0400

    feat: auto deploy

---
+++ build.js
@@ -0,0 +1,64 @@
+const aws = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
+const cheerio = require('cheerio');
+const shelljs = require('shelljs');
+const mime = require('content-type-mime');
+const globby = require('globby');
+
+const S3 = new aws.S3();
+const stdin = process.stdin;
+const css = [];
+
+stdin.on('data', data => css.push(data));
+
+stdin.on('end', () => {
+  const html = fs.readFileSync('./index.html').toString();
+  const $ = cheerio.load(html);
+  const processed = css.map(x => x.toString()).join('');
+  $('link').remove();
+  $('style').empty().html(processed);
+
+  S3.putObject(
+    {
+      Body: $.html(),
+      Key: 'index.html',
+      Bucket: 'mvp-demo',
+      ContentType: 'text/html',
+      ACL: 'public-read',
+    },
+    (err, data) => {
+      console.log(err, data);
+      if (!err) {
+        shelljs.exec('git checkout index.html');
+      }
+    }
+  );
+});
+
+const upload = (file, i) =>
+  new Promise((resolve, reject) => {
+    S3.putObject(
+      {
+        Body: fs.readFileSync(file),
+        Key: file.split('./')[1],
+        Bucket: 'mvp-demo',
+        ContentType: mime(path.basename(file)),
+        ACL: 'public-read',
+      },
+      (err, data) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      }
+    );
+  });
+
+const assets = globby.sync('./assets/*');
+const dist = globby.sync('./dist/*');
+
+Promise.all([
+  assets.filter(x => path.parse(x).ext !== '.css').forEach(upload),
+  dist.forEach(upload),
+]).then(() => console.log('done'));
